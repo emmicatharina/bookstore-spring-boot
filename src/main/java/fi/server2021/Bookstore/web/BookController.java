014f2d3cd27ad2b835738fd574d8fb70fcdbda1a
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-09-10 14:45:14 +0300

    Add create functionality

--- src/main/java/fi/server2021/Bookstore/web/BookController.java
+++ src/main/java/fi/server2021/Bookstore/web/BookController.java
@@ -1,12 +1,11 @@
 package fi.server2021.Bookstore.web;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fi.server2021.Bookstore.domain.Book;
 import fi.server2021.Bookstore.domain.BookRepository;
@@ -15,15 +14,29 @@ import fi.server2021.Bookstore.domain.BookRepository;
 public class BookController {
 	@Autowired
 	private BookRepository repository;
-	
-	ArrayList<Book> books = new ArrayList<>();
-	 
+		
+	// List all books
 	@GetMapping("/booklist")
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
 	
+	// Add new book
+	@GetMapping("/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	// Save new book
+	@PostMapping("/save")
+	public String saveBook(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	// Delete book
 	@GetMapping("/delete/{id}")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
