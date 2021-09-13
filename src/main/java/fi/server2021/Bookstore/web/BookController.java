1b76856ddf2e9d5e24084859e13b1e7744c08288
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-09-13 20:21:56 +0300

    Add edit functionality

--- src/main/java/fi/server2021/Bookstore/web/BookController.java
+++ src/main/java/fi/server2021/Bookstore/web/BookController.java
@@ -42,5 +42,12 @@ public class BookController {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
+	
+	//Edit book
+	@GetMapping("edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook";
+	}
 
 }
